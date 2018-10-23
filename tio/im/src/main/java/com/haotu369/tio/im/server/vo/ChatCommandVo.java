264df4219d908e1f62ff0777acf91011c846e010
shenjian <sjshenjian@outlook.com>
2018-10-23 19:34:16 +0800

    protobuf生成chat所需类

---
+++ tio/im/src/main/java/com/haotu369/tio/im/server/vo/ChatCommandVo.java
@@ -0,0 +1,58 @@
+package com.haotu369.tio.im.server.vo;
+
+import com.haotu369.tio.im.common.packets.Client;
+
+/**
+ * @author Jian Shen
+ * @version V1.0
+ * @date 2018/10/22
+ */
+public class ChatCommandVo {
+
+    private String initText;
+    private String  afterText;
+    private boolean privateChat = true;
+    private boolean command = true;
+
+    private Client fromClient;
+
+    public String getInitText() {
+        return initText;
+    }
+
+    public void setInitText(String initText) {
+        this.initText = initText;
+    }
+
+    public String getAfterText() {
+        return afterText;
+    }
+
+    public void setAfterText(String afterText) {
+        this.afterText = afterText;
+    }
+
+    public boolean isPrivateChat() {
+        return privateChat;
+    }
+
+    public void setPrivateChat(boolean privateChat) {
+        this.privateChat = privateChat;
+    }
+
+    public boolean isCommand() {
+        return command;
+    }
+
+    public void setCommand(boolean command) {
+        this.command = command;
+    }
+
+    public Client getFromClient() {
+        return fromClient;
+    }
+
+    public void setFromClient(Client fromClient) {
+        this.fromClient = fromClient;
+    }
+}
